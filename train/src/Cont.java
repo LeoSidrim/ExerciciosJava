734a5b729cfe3e77ec85af61b5258c36aee97974
Leonardo Sidrim <leonardosidrim2005@gmail.com>
2025-03-17 13:06:40 -0300

    More questions resolved

---
+++ train/src/Cont.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Cont {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i =1; i<=x;i++) {
+            if (i % 2 ==1) {
+                System.out.println("Número : " + i);
+            }
+
+        }
+        sc.close();
+        }
+
+    }
+// contador de número ímpares
---
+++ train/src/questão3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class questão3 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira quantidade N de números: ");
+        int n = sc.nextInt();
+        int sum=0;
+        int sum2=0;
+
+        for (int i=0; i<n; i++){
+            int x = sc.nextInt();
+            if(x>=10 && x<=20){
+                sum++;
+            } else{
+                sum2++;
+            }
+        }
+        System.out.println(sum+" in");
+        System.out.println(sum2+" out");
+        sc.close();
+    }
+} // In e Out
