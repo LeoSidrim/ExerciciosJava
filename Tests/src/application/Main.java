fae2c0883bef29a170c6d653fc0545e804059534
Leonardo Sidrim <leonardosidrim2005@gmail.com>
2025-04-07 08:22:51 -0300

    O.O training

--- Tests/src/application/Main.java
+++ Tests/src/application/Main.java
@@ -1,20 +1,20 @@
 package application;
-import entities.Rectangle;
-import java.awt.desktop.SystemSleepEvent;
 import java.util.Scanner;
-
+import entities.Employee;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Rectangle rectangle = new Rectangle();
-
-        System.out.println("Enter the rectangle width and height: ");
-        rectangle.width = sc.nextDouble();
-        rectangle.height = sc.nextDouble();
-
-
-
-
+        Employee employee = new Employee();
+        System.out.print("Name: ");
+        employee.name = (sc.nextLine());
+        System.out.print("Gross salary: ");
+        employee.grossSalary = (sc.nextDouble());
+        System.out.print("tax: ");
+        employee.tax = (sc.nextDouble());
+        System.out.println("Employee: "+ employee.name+", $" +employee.netSalary());
+        System.out.print("Which percentage to increase salary? ");
+        employee.increaseSalary((sc.nextDouble()));
+        System.out.println("Updated data: "+ employee.name+", $"+employee.grossSalary);
         sc.close();
     }
 }
